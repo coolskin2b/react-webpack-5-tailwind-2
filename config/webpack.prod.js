ef7b38823b4241496f118eb05b13da021cc7bfc1
Laurent Raffalli <35670661+coolskin2b@users.noreply.github.com>
2021-02-15 14:43:28 +0100

    Purge-Problem

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,4 +1,5 @@
 const paths = require('./paths')
+const glob = require('glob')
 const Dotenv = require('dotenv-webpack')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
@@ -6,6 +7,7 @@ const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -24,6 +26,9 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: 'styles/[name].[contenthash].css',
       chunkFilename: '[id].css',
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${paths.src}/**/*`, { nodir: true }),
     }),
   ],
   module: {
